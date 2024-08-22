840b8e2c935f2d161292c766c12e43bb595ef7ae
olivier SAINT-EVE <osainteve@gmail.com>
2024-08-22 10:50:20 +0200

    wip fini codage nouvelle version Save

--- ClientUtils/ViewModels/ExerciseVM.cs
+++ ClientUtils/ViewModels/ExerciseVM.cs
@@ -119,155 +119,205 @@ public partial class ExerciseVM : ObservableObject
     [RelayCommand]
     public async Task Save()
     {
-        if (CreationExercise())
+        Guid recoveryId = Guid.Empty;
+        
+        if (IsCreationExercise())
         {
-            
-            
-            
-            if (CreationDifficulty())
+            var savedNewExercise = await CreateExerciseAsync();
+            recoveryId = savedNewExercise.Id;
+
+            //the exercise is created
+            //because CreationExercise tests if datas are valid
+            if (IsCreationDifficulty() && savedNewExercise is not null)
+                await CreateDifficulty(savedNewExercise);
+        }
+        
+        else
+        {
+            if (IsModificationExercise())
             {
+                Exercise exercise = CurrentExercise;
+
+                if (IsModificationExerciseName())
+                    exercise = await ModifyExerciseName();
+
+                recoveryId = exercise.Id;
                 
+                if (IsCreationDifficulty())
+                    await CreateDifficulty(exercise);
+                else if (IsModificationDifficulty())
+                    await ModifyDifficulty(exercise);
             }
+
+            await Repository.SaveChangesAsync();
         }
-        else
+
+        bool IsCreationExercise()
         {
-            if (ModificationExercise())
+            var result = !string.IsNullOrEmpty(NewExerciseName);
+            return result;
+        }
+
+        bool IsCreationDifficulty()
+        {
+            var result = CurrentDifficulty is not null
+                         && CurrentDifficulty.DifficultyLevel > 0
+                         && CurrentDifficulty.DifficultyName.IsSome;
+            return result;
+        }
+
+        bool IsModificationExercise()
+        {
+            var result = Exercises.Any(e => e.Id == CurrentExercise.Id);
+            return result;
+        }
+
+        bool IsModificationExerciseName()
+        {
+            var result = !string.IsNullOrEmpty(ExistingExerciseName);
+            return result;
+        }
+
+        bool IsModificationDifficulty()
+        {
+            var result = CurrentDifficulty is not null
+                && CurrentExercise.ExerciseDifficulties
+                    .Any(d => d.Id == CurrentDifficulty.Id)
+                && CurrentDifficulty.DifficultyLevel > 0
+                && CurrentDifficulty.DifficultyName.IsSome;
+
+            return result;
+        }
+
+        async Task<Exercise> CreateExerciseAsync()
+        {
+            var newExercise = new Exercise()
             {
-                if (ModificationExerciseName())
-                {
-                    
-                    
-                    
-                    
-                }
-
-                if (AdditionDifficulty())
-                {
-                    
-                    
-                    
-                }
-                else if (ModificationDifficulty())
-                {
-                    
-                    
-                    
-                }
-            }
-        }
-        
-        bool CreationExercise()
-        {
-            throw new NotImplementedException();
-        }
-        
-        bool CreationDifficulty()
-        {
-            throw new NotImplementedException();
-        }
-
-        bool ModificationExercise()
-        {
-            throw new NotImplementedException();
-        }
-        
-        bool ModificationExerciseName()
-        {
-            throw new NotImplementedException();
-        }
-
-        bool AdditionDifficulty()
-        {
-            throw new NotImplementedException();
-        }
-
-        bool ModificationDifficulty()
-        {
-            throw new NotImplementedException();
-        }
-        
-        var existingExerciseNameEmptyOrSame =
-            string.IsNullOrEmpty(ExistingExerciseName)
-            || ExistingExerciseName.Equals(CurrentExercise.ExerciseName);
-
-        var currentDifficultyZeroOrNewOrSame =
-            CurrentDifficulty.DifficultyLevel == 0
-            || (CurrentDifficulty.DifficultyLevel ==
-                (CurrentExercise.ExerciseDifficulties.FirstOrDefault(d => d.Id == CurrentDifficulty.Id)
-                    ?.DifficultyLevel ?? -1)
-                && CurrentDifficulty.DifficultyName.Equals(
-                    CurrentExercise.ExerciseDifficulties.FirstOrDefault(d => d.Id == CurrentDifficulty.Id)
-                        ?.DifficultyName ?? "µ*ù"
-                ));
-
-        if (CurrentExercise is not null
-            && existingExerciseNameEmptyOrSame && currentDifficultyZeroOrNewOrSame)
-            return;
-
-        var exerciseInDatabase = Repository.Query<Exercise>()
-            .Where(e => e.Id == CurrentExercise.Id)
-            .Include(e => e.ExerciseDifficulties)
-            .FirstOrDefault();
-
-        // if (exerciseInDatabase is not null &&
-        //     exerciseInDatabase.ExerciseName.Equals(CurrentExercise.ExerciseName) &&
-        //     exerciseInDatabase.ExerciseDifficulties.Any(oneDifficulty =>
-        //         oneDifficulty == CurrentDifficulty))
-        //     return;
-
-        var recoveryExerciseId = Guid.Empty;
-
-        if (exerciseInDatabase is not null)
-        {
-            recoveryExerciseId = exerciseInDatabase.Id;
-            //l'exercice existe déjà
-            // changement de nom?
-            if (!ExistingExerciseName.Equals(exerciseInDatabase.ExerciseName)
-                && !string.IsNullOrEmpty(ExistingExerciseName)
-                && !Exercises.Any(e => e.ExerciseName.Equals(ExistingExerciseName)))
-                exerciseInDatabase.ExerciseName = ExistingExerciseName;
-
-            // ajout de difficulté?
-            if (exerciseInDatabase.ExerciseDifficulties.All(diff => diff.Id != CurrentDifficulty.Id)
-                && CurrentDifficulty.DifficultyLevel != 0)
-                exerciseInDatabase.ExerciseDifficulties.Add(CurrentDifficulty);
-
-            await Repository.LikeUpdateAsync(exerciseInDatabase);
-            await Repository.SaveChangesAsync(CancellationToken.None);
-        }
-        else
-        {
-            //nouvel exercice
-
-            //on veut en créer un avec un nom existant? refusé
-            var exerciseWithSameNameInDatabase = Repository.Query<Exercise>()
-                .Where(e => e.ExerciseName.Equals(NewExerciseName))
-                .Include(e => e.ExerciseDifficulties)
-                .FirstOrDefault();
-
-            if (exerciseWithSameNameInDatabase is not null)
-                return;
-
-            recoveryExerciseId = CurrentExercise.Id;
-
-            //autre exercice, on lui ajoute sa difficulté
-            Exercise newExercise = new()
-            {
-                Id = CurrentExercise.Id,
+                Id = Guid.NewGuid(),
                 ExerciseName = NewExerciseName,
-                ExerciseDifficulties = new()
+                ExerciseDifficulties = []
             };
-            if (CurrentDifficulty.DifficultyLevel != 0)
-                newExercise.ExerciseDifficulties.Add(CurrentDifficulty);
 
             await Repository.AddAsync(newExercise);
             await Repository.SaveChangesAsync(CancellationToken.None);
+            await Repository.ReloadAsync();
+            var savedExercise = Repository.Query<Exercise>()
+                .Include(e => e.ExerciseDifficulties)
+                .FirstOrDefault(e => e.Id == newExercise.Id);
+            
+            return savedExercise;
         }
+
+        async Task<ExerciceDifficulty> CreateDifficulty(Exercise savedNewExercise)
+        {
+            CurrentDifficulty.Exercice = savedNewExercise;
+            await Repository.AddAsync(CurrentDifficulty);
+            await Repository.SaveChangesAsync(CancellationToken.None);
+            await Repository.ReloadAsync();
+            var savedDifficulty = Repository.Query<ExerciceDifficulty>()
+                .Include(d => d.Exercice)
+                .FirstOrDefault(d => d.Id == CurrentDifficulty.Id);
+            
+            return savedDifficulty;
+        }
+
+        async Task<Exercise> ModifyExerciseName()
+        {
+            CurrentExercise.ExerciseName = ExistingExerciseName;
+            await Repository.LikeUpdateAsync(CurrentExercise);
+            return CurrentExercise;
+        }
+
+        async Task ModifyDifficulty(Exercise exercise)
+        {
+            CurrentDifficulty.Exercice = exercise;
+            await Repository.LikeUpdateAsync(CurrentDifficulty);
+        }
+
+        //*********************************
+
+        // var existingExerciseNameEmptyOrSame =
+        //     string.IsNullOrEmpty(ExistingExerciseName)
+        //     || ExistingExerciseName.Equals(CurrentExercise.ExerciseName);
+        //
+        // var currentDifficultyZeroOrNewOrSame =
+        //     CurrentDifficulty.DifficultyLevel == 0
+        //     || (CurrentDifficulty.DifficultyLevel ==
+        //         (CurrentExercise.ExerciseDifficulties.FirstOrDefault(d => d.Id == CurrentDifficulty.Id)
+        //             ?.DifficultyLevel ?? -1)
+        //         && CurrentDifficulty.DifficultyName.Equals(
+        //             CurrentExercise.ExerciseDifficulties.FirstOrDefault(d => d.Id == CurrentDifficulty.Id)
+        //                 ?.DifficultyName ?? "µ*ù"
+        //         ));
+        //
+        // if (CurrentExercise is not null
+        //     && existingExerciseNameEmptyOrSame && currentDifficultyZeroOrNewOrSame)
+        //     return;
+        //
+        // var exerciseInDatabase = Repository.Query<Exercise>()
+        //     .Where(e => e.Id == CurrentExercise.Id)
+        //     .Include(e => e.ExerciseDifficulties)
+        //     .FirstOrDefault();
+        //
+        // // if (exerciseInDatabase is not null &&
+        // //     exerciseInDatabase.ExerciseName.Equals(CurrentExercise.ExerciseName) &&
+        // //     exerciseInDatabase.ExerciseDifficulties.Any(oneDifficulty =>
+        // //         oneDifficulty == CurrentDifficulty))
+        // //     return;
+        //
+        // var recoveryExerciseId = Guid.Empty;
+        //
+        // if (exerciseInDatabase is not null)
+        // {
+        //     recoveryExerciseId = exerciseInDatabase.Id;
+        //     //l'exercice existe déjà
+        //     // changement de nom?
+        //     if (!ExistingExerciseName.Equals(exerciseInDatabase.ExerciseName)
+        //         && !string.IsNullOrEmpty(ExistingExerciseName)
+        //         && !Exercises.Any(e => e.ExerciseName.Equals(ExistingExerciseName)))
+        //         exerciseInDatabase.ExerciseName = ExistingExerciseName;
+        //
+        //     // ajout de difficulté?
+        //     if (exerciseInDatabase.ExerciseDifficulties.All(diff => diff.Id != CurrentDifficulty.Id)
+        //         && CurrentDifficulty.DifficultyLevel != 0)
+        //         exerciseInDatabase.ExerciseDifficulties.Add(CurrentDifficulty);
+        //
+        //     await Repository.LikeUpdateAsync(exerciseInDatabase);
+        //     await Repository.SaveChangesAsync(CancellationToken.None);
+        // }
+        // else
+        // {
+        //     //nouvel exercice
+        //
+        //     //on veut en créer un avec un nom existant? refusé
+        //     var exerciseWithSameNameInDatabase = Repository.Query<Exercise>()
+        //         .Where(e => e.ExerciseName.Equals(NewExerciseName))
+        //         .Include(e => e.ExerciseDifficulties)
+        //         .FirstOrDefault();
+        //
+        //     if (exerciseWithSameNameInDatabase is not null)
+        //         return;
+        //
+        //     recoveryExerciseId = CurrentExercise.Id;
+        //
+        //     //autre exercice, on lui ajoute sa difficulté
+        //     Exercise newExercise = new()
+        //     {
+        //         Id = CurrentExercise.Id,
+        //         ExerciseName = NewExerciseName,
+        //         ExerciseDifficulties = new()
+        //     };
+        //     if (CurrentDifficulty.DifficultyLevel != 0)
+        //         newExercise.ExerciseDifficulties.Add(CurrentDifficulty);
+        //
+        //     await Repository.AddAsync(newExercise);
+        //     await Repository.SaveChangesAsync(CancellationToken.None);
+        // }
 
         await LoadExercises();
         CurrentDifficulty = new(0, "Kg");
         SelectedExercise = Exercises
-            .FirstOrDefault(e => e.Id == recoveryExerciseId);
+            .FirstOrDefault(e => e.Id == recoveryId);
     }
 
     [RelayCommand]
